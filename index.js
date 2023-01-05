2a1598ffaf4c178217791c182f5deca58cc7b291
xreyaaa <shreyashrestha984@gmail.com>
2023-01-05 02:41:02 -0800

    learning basic javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+//var x=9;
+//console.log(x);
+//x=12;
+//console.log(x);
+//var x=7*7;
+//console.log(x);
+// let y=55;
+// console.log(y);
+// y=56;
+// console.log(y);
+
+// const PI=3.14;
+// console.log(PI);
+// PI=3.89;
+// console.log(PI);
+// const PI=90;
+
+// const x=90;
+// console.log(x);
+// console.log(typeof x);
+// let bool=true;
+// console.log(bool);
+// console.log(typeof bool);
+
+// let x=0;
+// let y=67;
+// let z=true;
+// let result=x+y+z;
+// console.log(result);
+// let name="opod dnm";
+// let y=90;
+// let find=name+y;
+// console.log(find);
+
+const name="dip";
+const surname="hubj";
+console.log(name+" "+surname);
+const myGreeting= (`my name is ${name} ${surname}.`);
+console.log(`hello vereyone ${myGreeting}`);
