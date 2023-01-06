d43692dc8d255d51ee29bfcacb35c87ae5cb366b
xreyaaa <shreyashrestha984@gmail.com>
2023-01-06 02:21:13 -0800

    type conversing concept

--- index.js
+++ index.js
@@ -38,7 +38,47 @@
 // const myGreeting= (`my name is ${name} ${surname}.`);
 // console.log(`hello vereyone ${myGreeting}`);
 
-let name="bfehbf ejfeijfb";
-console.log(name);
-console.log(typeof name);
-console.log(typeof Number(name));
+// let name="bfehbf ejfeijfb";
+// console.log(name);
+// console.log(typeof name);
+// console.log(typeof Number(name));
+
+// //type convertion and string methds day 3
+// let number = 678;
+//  console.log(number);
+// // console.log(typeof String(number));
+// console.log(typeof number .toString());
+
+//string method
+let str ="hello, world!";
+let y=" lol snkn nksn";
+let z="   how is your life    "
+
+// //charAt
+// console.log(str.charAt(0))
+// //length
+// console.log(str.length)
+// //concat
+// console.log(str.concat(y));//value jodne str rw y ko
+// //replace
+// console.log(str.replace("hello","hi")) //hello ko thau ma hi lai repplace gardine
+// //indexof
+// console.log(str.indexOf("o")); //hello ma o ko index nikalne 01234 (first ko)
+// //lastindexOf
+// console.log(str.lastIndexOf("l"));// autai letter duita xa vane(last ko)
+// //includes
+// console.log(str.includes("hello"));// tyo word line ma xa vane true auxa natra false auxa jastai hello mathi ko lina ma xa
+// //split
+// console.log(str.split("l"));//break garxa l bata aru world haru
+// //tolowercase
+// console.log(str.toUpperCase());
+
+//trim
+//console.log(z.trim());//space haru hatauxa agadi paxaid ko space lai matra string paxi ko space lai gardaina
+
+//slice
+console.log(str.slice(1,4));//start dekhi end samma katxa index lai output hell0123 4 end lai gandaina
+
+
+
+
